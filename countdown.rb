8b292f43900c8223d946b4fff57068d2f6772d1e
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-06 19:28:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(number)
    "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(num_secs)
+  while number >= 1
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+
+ "HAPPY NEW YEAR!"
+
+end
